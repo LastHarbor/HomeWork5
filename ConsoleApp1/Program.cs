db61c294204bdc37b591715e4eaddefa2fd6136a
Антон Кулакевич <antonkulakevich@mail.ru>
2022-07-08 16:03:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ConsoleApp1;
+
+internal class Program
+{
+     static string GetString ()
+     {
+         Console.WriteLine("Введите предложение:");
+         string inputText = Console.ReadLine();
+         return inputText;
+     }
+
+     static string[] SplitText(string inputText)
+     {
+
+         string[] textArray = inputText.Split(' ');
+         foreach (var words in textArray)
+         {
+             Console.WriteLine($" {words} ");
+         }
+
+         return textArray;
+     }
+
+     static void Main()
+     {
+         SplitText(GetString());
+     }
+}
---
+++ HomeWork5.2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace HomeWork5._2
+{
+    internal class Program
+    {
+
+
+        static string[] SplitSentence()
+        {
+            Console.WriteLine("Введите предложение: ");
+            string sentence = Console.ReadLine();
+            string[] splitText = sentence.Split(' ');
+            foreach (var words in splitText)
+            {
+                Console.WriteLine($" {words} ");
+            }
+
+            return splitText;
+        }
+        static string[] ReverseSentence(string[] splitText)
+        {
+            Array.Reverse(splitText);
+            Console.WriteLine(" \n ");
+            foreach (var word in splitText)
+            {
+                Console.WriteLine($" {word} ");
+            }
+            return splitText;
+        }
+
+        static void Main()
+        {
+            ReverseSentence(SplitSentence());
+
+
+        }
+    }
+}
